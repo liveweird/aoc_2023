13dfd84d888617a736fea2e31ea58e9377f2dd5a
Sebastian Gebski <sebastian@gebski.net>
2023-12-15 21:49:53 +0100

    Day 05, part 2 (faster)

--- day05.py
+++ day05.py
@@ -5,7 +5,11 @@ from typing import Callable
 
 def get_seeds_part1(line: str) -> list:
     parsed_seeds: list = re.match(r"^seeds: (.+)$", line)
-    return parsed_seeds.group(1).split(" ")
+    strings: list = parsed_seeds.group(1).split(" ")
+
+    # convert the list of strings into the list of ints
+    seeds: list = list(map(int, strings))
+    return seeds
 
 
 def get_seeds_part2(line: str) -> list:
@@ -19,22 +23,45 @@ def get_seeds_part2(line: str) -> list:
     # iterate through each pair in pairs
     for pair in pairs:
         seeds = seeds.union(set(range(int(pair[0]), int(pair[0]) + int(pair[1]))))
-        print(".")
-
-    print("done")
 
     return list(seeds)
 
 
+def in_seeds_part1(line: str, checked: int) -> bool:
+    parsed_seeds: list = re.match(r"^seeds: (.+)$", line)
+    strings: list = parsed_seeds.group(1).split(" ")
+
+    # convert the list of strings into the list of ints
+    seeds: list = list(map(int, strings))
+    return checked in seeds
+
+
+def in_seeds_part2(line: str, checked: int) -> bool:
+    parsed_seeds: list = re.match(r"^seeds: (.+)$", line)
+    singles: list = parsed_seeds.group(1).split(" ")
+    pairs: list = list(zip(singles[::2], singles[1::2]))
+
+    # get the pairs sorted by the first element
+    pairs = sorted(pairs, key=lambda sorted_pair: int(sorted_pair[0]))
+
+    # iterate through each pair in pairs
+    for pair in pairs:
+        # if the checked number is in the range of the pair, return True
+        if int(pair[0]) <= checked <= int(pair[0]) + int(pair[1]):
+            return True
+
+    return False
+
+
 def day05_part1(file_name: str) -> int:
-    return day05(file_name, get_seeds_part1)
+    return day05b(file_name, in_seeds_part1)
 
 
 def day05_part2(file_name: str) -> int:
-    return day05(file_name, get_seeds_part2)
+    return day05b(file_name, in_seeds_part2)
 
 
-def day05(file_name: str, get_seeds: Callable[[str], int]) -> int:
+def day05a(file_name: str, get_seeds: Callable[[str], int]) -> int:
     # open the input file
     f: typing.TextIO = open(file_name, "r")
 
@@ -76,7 +103,7 @@ def day05(file_name: str, get_seeds: Callable[[str], int]) -> int:
     # go through all the seeds
     for seed in seeds:
         # go through all the mappings
-        subject: int = int(seed)
+        subject: int = seed
         for mapping in mappings:
             # go through all the mapping rules
             for rule in mapping:
@@ -89,6 +116,64 @@ def day05(file_name: str, get_seeds: Callable[[str], int]) -> int:
         subjects.append(subject)
 
     return min(subjects)
+
+
+def day05b(file_name: str, in_seeds: Callable[[str, int], int]) -> int:
+    # open the input file
+    f: typing.TextIO = open(file_name, "r")
+
+    # read seeds line from the file
+    seed_line: str = f.readline()
+
+    # read the empty line
+    _ = f.readline()
+
+    # create a list of empty lists
+    mappings: list = []
+
+    # read the section header
+    line: str = f.readline()
+
+    while line != "":
+        parsed_header: list = re.match(r"^(\w+)-to-(\w+) map:$", line)
+        from_name: str = parsed_header.group(1)
+        to_name: str = parsed_header.group(2)
+        mapping: list = []
+
+        line = f.readline()
+
+        # read line from file "f" until it's empty line or EOF
+        while line != "" and line != "\n":
+            parsed_line: list = re.match(r"^(\d+) (\d+) (\d+)$", line)
+            dest_idx: int = int(parsed_line.group(1))
+            source_idx: int = int(parsed_line.group(2))
+            qty: int = int(parsed_line.group(3))
+            mapping.append((source_idx, qty, dest_idx))
+            line = f.readline()
+
+        mappings.append(sorted(mapping, key=lambda trio: trio[2]))
+        line = f.readline()
+
+    idx: int = 0
+
+    # go through all the tuples in the final_mapping
+    while True:
+        subject: int = idx
+
+        # go through all the mappings, in the reverse order
+        for mapping in reversed(mappings):
+            # go through all the mapping rules
+            for rule in mapping:
+                # if the rule applies to the value, use it
+                if rule[2] <= subject <= (rule[2] + rule[1] - 1):
+                    subject = rule[0] + (subject - rule[2])
+                    break
+
+        # if the subject is in the seeds, return it
+        if in_seeds(seed_line, subject):
+            return idx
+
+        idx += 1
 
 
 def test_day05_part1a():
@@ -104,4 +189,4 @@ def test_day05_part2a():
 
 
 def test_day05_part2b():
-    assert day05_part2("./input/day05b.txt") == 46
+    assert day05_part2("./input/day05b.txt") == 20358599
