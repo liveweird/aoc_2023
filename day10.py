c0680c48eb200b9186064a6773ad2cdfd69c5af3
Sebastian Gebski <sebastian@gebski.net>
2024-07-09 22:19:47 +0200

    Modified part 1, for directional traversal

--- day10.py
+++ day10.py
@@ -1,3 +1,13 @@
+from enum import Enum, auto
+
+
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
 class Maze:
     def __init__(self):
         self.maze = []
@@ -27,54 +37,76 @@ class Maze:
             return False
         return True
 
-    def find_potential_neighbors(self, i: int, j: int) -> list:
-        potentials = []
+    def find_next(self, i: int, j: int) -> tuple:
         symbol = self.maze[i][j]
         if symbol == "|":
-            if self.is_pos_legit(i-1, j):
-                potentials.append((i-1, j))
-            if self.is_pos_legit(i+1, j):
-                potentials.append((i+1, j))
+            if self.is_pos_legit(i-1, j) and not self.is_visited((i-1, j)):
+                return i-1, j, Direction.UP
+            if self.is_pos_legit(i+1, j) and not self.is_visited((i+1, j)):
+                return i+1, j, Direction.DOWN
         elif symbol == "-":
-            if self.is_pos_legit(i, j-1):
-                potentials.append((i, j-1))
-            if self.is_pos_legit(i, j+1):
-                potentials.append((i, j+1))
+            if self.is_pos_legit(i, j-1) and not self.is_visited((i, j-1)):
+                return i, j-1, Direction.LEFT
+            if self.is_pos_legit(i, j+1) and not self.is_visited((i, j+1)):
+                return i, j+1, Direction.RIGHT
         elif symbol == "L":
-            if self.is_pos_legit(i-1, j):
-                potentials.append((i-1, j))
-            if self.is_pos_legit(i, j+1):
-                potentials.append((i, j+1))
+            if self.is_pos_legit(i-1, j) and not self.is_visited((i-1, j)):
+                return i-1, j, Direction.UP
+            if self.is_pos_legit(i, j+1) and not self.is_visited((i, j+1)):
+                return i, j+1, Direction.RIGHT
         elif symbol == "J":
-            if self.is_pos_legit(i-1, j):
-                potentials.append((i-1, j))
-            if self.is_pos_legit(i, j-1):
-                potentials.append((i, j-1))
+            if self.is_pos_legit(i-1, j) and not self.is_visited((i-1, j)):
+                return i-1, j, Direction.UP
+            if self.is_pos_legit(i, j-1) and not self.is_visited((i, j-1)):
+                return i, j-1, Direction.LEFT
         elif symbol == "7":
-            if self.is_pos_legit(i+1, j):
-                potentials.append((i+1, j))
-            if self.is_pos_legit(i, j-1):
-                potentials.append((i, j-1))
+            if self.is_pos_legit(i+1, j) and not self.is_visited((i+1, j)):
+                return i+1, j, Direction.DOWN
+            if self.is_pos_legit(i, j-1) and not self.is_visited((i, j-1)):
+                return i, j-1, Direction.LEFT
         elif symbol == "F":
-            if self.is_pos_legit(i+1, j):
-                potentials.append((i+1, j))
-            if self.is_pos_legit(i, j+1):
-                potentials.append((i, j+1))
-        return potentials
+            if self.is_pos_legit(i+1, j) and not self.is_visited((i+1, j)):
+                return i+1, j, Direction.DOWN
+            if self.is_pos_legit(i, j+1) and not self.is_visited((i, j+1)):
+                return i, j+1, Direction.RIGHT
+
+        return ()
 
     def find_two_paths(self) -> tuple:
         paths = []
+        dirs = set()
         (i, j) = self.start
         if self.is_pos_legit(i - 1, j) and self.maze[i - 1][j] in ("|", "7", "F"):
             paths.append((i - 1, j))
+            dirs.add(Direction.UP)
         if self.is_pos_legit(i + 1, j) and self.maze[i + 1][j] in ("|", "L", "J"):
             paths.append((i + 1, j))
+            dirs.add(Direction.DOWN)
         if self.is_pos_legit(i, j - 1) and self.maze[i][j - 1] in ("-", "L", "F"):
             paths.append((i, j - 1))
+            dirs.add(Direction.LEFT)
         if self.is_pos_legit(i, j + 1) and self.maze[i][j + 1] in ("-", "7", "J"):
             paths.append((i, j + 1))
+            dirs.add(Direction.RIGHT)
 
-        assert(len(paths) == 2)
+        assert len(paths) == 2
+
+        # replace the start with the correct symbol
+        if Direction.LEFT in dirs and Direction.RIGHT in dirs:
+            self.maze[i][j] = "-"
+        elif Direction.UP in dirs and Direction.DOWN in dirs:
+            self.maze[i][j] = "|"
+        elif Direction.LEFT in dirs and Direction.UP in dirs:
+            self.maze[i][j] = "J"
+        elif Direction.LEFT in dirs and Direction.DOWN in dirs:
+            self.maze[i][j] = "7"
+        elif Direction.RIGHT in dirs and Direction.UP in dirs:
+            self.maze[i][j] = "L"
+        elif Direction.RIGHT in dirs and Direction.DOWN in dirs:
+            self.maze[i][j] = "F"
+        else:
+            assert False
+
         return paths[0], paths[1]
 
     def is_visited(self, pos: tuple) -> bool:
@@ -82,9 +114,6 @@ class Maze:
 
     def add_to_visited(self, pos: tuple, dist: int):
         self.visited[pos] = dist
-
-    def filter_visited(self, potentials: list) -> list:
-        return [pos for pos in potentials if not self.is_visited(pos)]
 
 
 def day10_part1(file_name: str) -> int:
@@ -104,22 +133,47 @@ def day10_part1(file_name: str) -> int:
 
         # traverse left and right end
         dist += 1
-        maze.add_to_visited(path1, dist)
-        maze.add_to_visited(path2, dist)
+        maze.add_to_visited((path1[0], path1[1]), dist)
+        maze.add_to_visited((path2[0], path2[1]), dist)
 
-        pot1 = maze.find_potential_neighbors(path1[0], path1[1])
-        next1 = maze.filter_visited(pot1)
-        if len(next1) == 1:
-            path1 = next1[0]
-        else:
+        path1 = maze.find_next(path1[0], path1[1])
+        if len(path1) == 0:
             return dist
 
-        pot2 = maze.find_potential_neighbors(path2[0], path2[1])
-        next2 = maze.filter_visited(pot2)
-        if len(next2) == 1:
-            path2 = next2[0]
-        else:
+        path2 = maze.find_next(path2[0], path2[1])
+        if len(path2) == 0:
             return dist
+
+
+def day10_part2(file_name: str) -> int:
+    # read the maze from the file
+    maze = Maze()
+    maze.read_maze(file_name)
+    dist = 0
+    maze.find_start()
+    maze.add_to_visited(maze.start, dist)
+    (path, _) = maze.find_two_paths()
+
+    # traverse the whole maze, to collect the list of tiles
+    while True:
+        dist += 1
+        maze.add_to_visited(path, dist)
+
+        path = maze.find_next(path[0], path[1])
+        if len(path) == 0:
+            break
+
+    # pick the direction, start traversing the maze
+    lefties = set()
+    righties = set()
+    prev = len(maze.visited) - 1
+    curr = 0
+
+    # collect tiles on the left and right separately
+    # for the tiles on the left, use flooding algorithm
+    # for the tiles on the right, use flooding algorithm
+    # return the size of the set of the tiles that doesn't touch boundaries
+    return 0
 
 
 def test_day10_part1a():
@@ -140,3 +194,15 @@ def test_day10_part1d():
 
 def test_day10_part1e():
     assert day10_part1("./input/day10e.txt") == 6942
+
+
+def test_day10_part2a():
+    assert day10_part2("./input/day10f.txt") == 4
+
+
+def test_day10_part2b():
+    assert day10_part2("./input/day10g.txt") == 8
+
+
+def test_day10_part2c():
+    assert day10_part2("./input/day10h.txt") == 10
