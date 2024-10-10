5e4d80f15d38046c7bbe6c7dcdc50c1dd7855418
Sebastian Gebski <sebastian@gebski.net>
2024-10-10 19:59:34 +0200

    Day 17, init

---
+++ day17.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Day17:
+    @staticmethod
+    def part1(file_name: str) -> int:
+        pass
+
+    @staticmethod
+    def part2(file_name: str) -> int:
+        pass
+
+class Tests(unittest.TestCase):
+    def test_day17_part1a(self):
+        self.assertEqual(Day17.part1("input/day17a.txt"), 102)
+
+    def test_day17_part1b(self):
+        self.assertEqual(Day17.part1("input/day17b.txt"), 102)
